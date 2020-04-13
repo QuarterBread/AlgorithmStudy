37d4f9aacb69aba0b24dfbfc5ccb816c818eddd5
TaeSeongMin <mts7309@naver.com>
2020-04-13 17:28:22 +0900

    Update IronStick.java

--- Programmers/Stack_Queue/5쇠막대기/민태성/IronStick.java
+++ Programmers/Stack_Queue/5쇠막대기/민태성/IronStick.java
@@ -11,10 +11,10 @@ public class IronStick {
         public int solution(String arrangement) {
             int answer = 0;
             Stack<Character> stack = new Stack<>();
-
+   
             for (int i = 0; i < arrangement.length(); i++) {
                 char c = arrangement.charAt(i);
-                //stick start
+                //stick
                 if (c == '(') {
                     stack.push(c);
                 }
@@ -23,16 +23,12 @@ public class IronStick {
                     if (!stack.isEmpty() && stack.peek() == '('&& arrangement.charAt(i-1) != ')' ) {
                         stack.pop();
                         answer += stack.size();
-                    }
-                    //stick -1 이지만 스틱하나가 완성? 되서 answer++
+                    }//stick -1
                     else {
-                        if (c != ')') {
-                            stack.push(c);
-                        }
-                        else{
+                        
                             answer ++;
                             stack.pop();
-                        }
+                        
                     }
                 }
 
